b9d98e60b6fa5698793b6e2ef2d0a13d12942084
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-03 16:16:40 +0100

    Completed step 13

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,11 @@
 require "station"
 
 describe Station do
+  it "should have a zone attribute" do
+    expect(subject.zone).to be_between(1,5).inclusive
+  end
 
+  it "should have a name attribute" do
+    expect(subject.name).to be_between(0.0,1.0).inclusive
+  end
 end
