31232feaeeddbafe877f58f0d550161d1dc5ea52
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-03 16:22:39 +0100

    Refactored

--- lib/station.rb
+++ lib/station.rb
@@ -1,8 +1,8 @@
 class Station
   attr_reader :name, :zone
   
-  def initialize
-    @name = rand
-    @zone = rand(1..5)
+  def initialize (name = rand, zone = rand(1..5))
+    @name = name
+    @zone = zone
   end
 end
